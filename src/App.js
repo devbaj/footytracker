92ab4821a1bb082e201af6c429301fc9ef760b74
Brett Jory <dev.bjory@gmail.com>
2019-09-18 10:56:05 -0700

    initial API implementation online

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import secrets from './secrets';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accessToken: secrets.token
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <LiveMatches
+          token={this.state.accessToken}
+        ></LiveMatches>
+      </div>
   );
+  }
+}
+
+class LiveMatches extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    var url = 'http://api.football-data.org/v2/competitions',
+      options = {
+        headers: {
+          'X-Auth-Token': this.props.token
+        },
+        dataType: 'json',
+        method: 'GET'
+      };
+
+    fetch(url, options)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Live Matches</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
